e66c40aecc9d7c09468d8fd40fba22a1458ffed2
Luciano Repetti <67850074+luciano-repetti@users.noreply.github.com>
2024-05-09 00:33:16 -0300

    Revert "[ADD] practicando ando"

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,6 @@
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-
-import { CssBaseline } from '@mui/material';
-
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-    return (
-        <>
-            <CssBaseline />
-            <Component {...pageProps} />
-        </>)
+  return <Component {...pageProps} />;
 }
